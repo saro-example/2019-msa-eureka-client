895e953217cd2d6c0d60866d0f2e01ffe0b437ba
PARK Yong Seo <j@saro.me>
2019-07-20 21:25:19 +0900

    init

---
+++ src/main/java/me/saro/example/msaeurekaclient/MsaEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package me.saro.example.msaeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaEurekaClientApplication.class, args);
+	}
+
+}
