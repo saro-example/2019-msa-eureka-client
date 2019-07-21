58eee5b69234479bb8282bfc8feaf9189029ade8
PARK Yong Seo <j@saro.me>
2019-07-22 01:03:00 +0900

    gitignore

--- src/main/java/me/saro/example/msaeurekaclient/ExampleController.java
+++ src/main/java/me/saro/example/msaeurekaclient/ExampleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,24 @@ public class ExampleController {
     @Value("${spring.application.name}")
     private String appName;
 
+    String id;
+
+    String getId() {
+        if (id == null) {
+            id = Integer.toString ((int)(Math.random() * 1000));
+        }
+        return id;
+    }
+
     @GetMapping("/hello")
     String hello() {
-        return "hello, i am from " + eurekaClient.getApplication(appName).getName();
+        System.out.println("call " + id);
+        return "hello, i am from " + eurekaClient.getApplication(appName).getName() + " : " + getId();
     }
+
+//    @RequestMapping("/health")
+//    public String health() {
+//        System.out.println("헬스체크");
+//        return "OK";
+//    }
 }
